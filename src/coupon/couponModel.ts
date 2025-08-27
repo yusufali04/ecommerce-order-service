b882d05b73f053bbda7ab51080613e13f1ecb481
Yusuf <yusufali.5094@gmail.com>
2025-08-27 21:36:19 +0530

    add timestamps in couponModel

--- src/coupon/couponModel.ts
+++ src/coupon/couponModel.ts
@@ -7,6 +7,6 @@ const couponSchema = new mongoose.Schema<Coupon>({
     discount: { type: Number, required: true },
     validUpto: { type: Date, required: true },
     tenantId: { type: Number, required: true }
-})
+}, { timestamps: true })
 
 export const CouponModel = mongoose.model("Coupon", couponSchema);
