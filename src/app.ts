bb779a4fecbb632408d3b497498072ab0bb7d5cb
Yusuf <yusufali.5094@gmail.com>
2025-09-03 22:36:32 +0530

    allow Idempotency-Key header in cors

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie", "Cookie"],
+  allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie", "Cookie", "Idempotency-Key"],
 };
 app.use(cors(corsOptions));
 app.use(cookieParser());
